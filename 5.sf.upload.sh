de3875a96c56ddd5cb76e9f9608e0c566dcf3300
gnuhub <gnuhub@gmail.com>
2022-11-06 12:30:46 +0800

    debug upload auto commit

--- 5.sf.upload.sh
+++ 5.sf.upload.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 #打开执行过程显示
-set -x
+set +x
 #显示设置环境变量 CMD_PATH当前脚本所在目录
 export CMD_PATH=$(cd `dirname $0`; pwd)
 export PROJECT_NAME="${CMD_PATH##*/}"
 
 cd $CMD_PATH
 mkdir -p ~/.ssh/
-echo "${secrets.MY_SF_SSH}" > ~/.ssh/id_rsa
+echo "${MY_SF_SSH}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 cp -fv known_hosts ~/.ssh/known_hosts
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "frs.sourceforge.net"
