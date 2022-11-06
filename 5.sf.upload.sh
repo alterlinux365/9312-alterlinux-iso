35c80521146cef708b82a4a8840bf83c3c00dcee
gnuhub <gnuhub@gmail.com>
2022-11-06 12:51:10 +0800

    debug upload auto commit

--- 5.sf.upload.sh
+++ 5.sf.upload.sh
@@ -10,18 +10,11 @@ cd $CMD_PATH
 env
 pwd
 whoami
-mkdir -p /root/.ssh/
-cat>/root/.ssh/id_rsa<<EOF
-${MY_SF_SSH}
-EOF
 
-chmod 600 /root/.ssh/id_rsa
 cp -fv known_hosts /root/.ssh/known_hosts
 ssh-keygen -f "/root/.ssh/known_hosts" -R "frs.sourceforge.net"
 ssh-keyscan "frs.sourceforge.net" >> /root/.ssh/known_hosts
 cat /root/.ssh/known_hosts
-ssh-agent bash
-ssh-add
 mkdir -p $GITHUB_REF_NAME
 rsync -avzP -e "ssh -i  /root/.ssh/id_rsa" ./$GITHUB_REF_NAME/  gnuhub@frs.sourceforge.net:/home/frs/project/alterlinux365/$GITHUB_REF_NAME/
 rsync -avzP -e "ssh -i  /root/.ssh/id_rsa" ./out/  gnuhub@frs.sourceforge.net:/home/frs/project/alterlinux365/$GITHUB_REF_NAME/$GITHUB_RUN_NUMBER/
