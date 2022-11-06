a741d245df4b3cc06e0be2bc398c67adee4f7c32
gnuhub <gnuhub@gmail.com>
2022-11-06 20:12:51 +0800

    auto commit

--- system/aur.sh
+++ system/aur.sh
@@ -36,8 +36,7 @@ Server = https://america.mirror.pkgbuild.com/\$repo/os/\$arch
 
 EOF
 
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+
 
 
 
@@ -47,6 +46,11 @@ pacman-key --init
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman --noconfirm -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+cat /etc/pacman.conf | grep chaotic
+if [ $? == 1 ];then
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+fi
 pacman -Syyu --noconfirm
 
 trap 'exit 1' 1 2 3 15
