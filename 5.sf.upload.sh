02496adb5f65f2747487e089acb3eb400735ca44
gnuhub <gnuhub@gmail.com>
2022-11-06 12:34:38 +0800

    debug upload auto commit

--- 5.sf.upload.sh
+++ 5.sf.upload.sh
@@ -7,15 +7,18 @@ export CMD_PATH=$(cd `dirname $0`; pwd)
 export PROJECT_NAME="${CMD_PATH##*/}"
 
 cd $CMD_PATH
-mkdir -p ~/.ssh/
-echo "${MY_SF_SSH}" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-cp -fv known_hosts ~/.ssh/known_hosts
+env
+pwd
+whoami
+mkdir -p /root/.ssh/
+echo "${MY_SF_SSH}" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+cp -fv known_hosts /root/.ssh/known_hosts
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "frs.sourceforge.net"
-ssh-keyscan "frs.sourceforge.net" >> ~/.ssh/known_hosts
-cat ~/.ssh/known_hosts
+ssh-keyscan "frs.sourceforge.net" >> /root/.ssh/known_hosts
+cat /root/.ssh/known_hosts
 ssh-agent bash
 ssh-add
 mkdir -p $GITHUB_REF_NAME
-rsync -avzP -e "ssh -i  ~/.ssh/id_rsa" ./$GITHUB_REF_NAME/  gnuhub@frs.sourceforge.net:/home/frs/project/alterlinux365/$GITHUB_REF_NAME/
-rsync -avzP -e "ssh -i  ~/.ssh/id_rsa" ./out/  gnuhub@frs.sourceforge.net:/home/frs/project/alterlinux365/$GITHUB_REF_NAME/$GITHUB_RUN_NUMBER/
+rsync -avzP -e "ssh -i  /root/.ssh/id_rsa" ./$GITHUB_REF_NAME/  gnuhub@frs.sourceforge.net:/home/frs/project/alterlinux365/$GITHUB_REF_NAME/
+rsync -avzP -e "ssh -i  /root/.ssh/id_rsa" ./out/  gnuhub@frs.sourceforge.net:/home/frs/project/alterlinux365/$GITHUB_REF_NAME/$GITHUB_RUN_NUMBER/
